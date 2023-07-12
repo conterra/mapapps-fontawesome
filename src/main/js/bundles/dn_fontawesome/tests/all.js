f82687fc1da2c4fbf0b54ce3506ae937ebf58664
Julian Kuhlmann <j.kuhlmann@conterra.de>
2023-07-12 11:03:49 +0200

    Update dev setup to latest mapapps-4-developers sample

--- src/main/js/bundles/dn_fontawesome/tests/all.js
+++ src/main/js/bundles/dn_fontawesome/tests/all.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 con terra GmbH (info@conterra.de)
+ * Copyright (C) 2023 con terra GmbH (info@conterra.de)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,4 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import i18n from "dojo/i18n!./nls/bundle"

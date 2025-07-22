2ccec41b40a5bd8c339df5593c2c8a82fd79f71e
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-07-22 16:56:11 +0200

    Update sample app

---
+++ src/main/js/apps/sample/nls/bundle.js
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2025 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module.exports = {
+    root: {
+        apptitle: "Font Awesome Sample",
+        map: {
+            koeln1: {
+                title: "Basic Data",
+                districts: {
+                    title: "City Districts",
+                    text: "Cologne's city district <b>{STV_NAME}</b>."
+                },
+                boroughs: {
+                    title: "Boroughs",
+                    text: "Boroughs <b>{NAME}</b> is located in Cologne's precints {STADTBEZIR}."
+                },
+                precints: {
+                    title: "Precints",
+                    text: "Cologne's precint {NAME}."
+                }
+            },
+            koeln2: {
+                title: "Education and Culture",
+                description: "List of all libraries, museums and schools in Cologne.",
+                libraries: {
+                    title: "Libraries"
+                },
+                museums: {
+                    title: "Museums",
+                    text: "Museum <b>{NAME}</b> is located in Cologne's boroughs {STADTTEIL}."
+                },
+                schools: {
+                    title: "Schools"
+                }
+            },
+            koeln3: {
+                title: "Recreation",
+                sights: {
+                    title: "Tourist Attractions",
+                    titleSingle: "Tourist Attraction",
+                    text: "Tourist attraction <b>{NAME}</b> is located in Cologne's borough {STADTTEIL}."
+                },
+                playgrounds: {
+                    title: "Playgrounds- and Sports Areas",
+                    text: "<b>{Spielplatzname}</b> is located in Cologne's borough {Stadtteil}.",
+                    baskets: "Basketball Baskets",
+                    goals: "Soccer Goals",
+                    tables: "Ping-Pong Tables",
+                    walls: "Goal Wall",
+                    skate: "Skating",
+                    misc: "Miscellaneous"
+                },
+                places: {
+                    title: "Places of Event",
+                    titleSingle: "Place of Event",
+                    text: "<b>{NAME}</b> is a {expression/carrier} place of event."
+                }
+            },
+            basemaps: {
+                gray: "Street Map (gray)",
+                streets: "Street Map",
+                topo: "Topographical Map",
+                hybrid: "Aerial (hybrid)"
+            }
+        },
+        tools: {
+            drawerLeft: "Tools",
+            measuring: "Measuring Tools"
+        },
+        common: {
+            number: "Number",
+            area: "Area [ha]",
+            totalArea: "Percentage of the total area [%]",
+            name: "Name",
+            provider: "Provider",
+            address: "Address",
+            furtherinfo: "Further Information",
+            precint: "Precint",
+            district: "District",
+            private: "privater",
+            municipal: "municipal",
+            zip: "Zip code",
+            type: "Type"
+        }
+    },
+    "de": true
+};
---
+++ src/main/js/apps/sample/nls/de/bundle.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2025 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module.exports = {
+    apptitle: "Font Awesome Sample",
+    map: {
+        koeln1: {
+            title: "Basisdaten",
+            districts: {
+                title: "Stadtviertel",
+                text: "Das K\xF6lner Stadtviertel <b>{STV_NAME}</b>."
+            },
+            boroughs: {
+                title: "Stadtteile",
+                text: "Der Stadtteil <b>{NAME}</b> liegt im K\xF6lner Stadtbezirk {STADTBEZIR}."
+            },
+            precints: {
+                title: "Stadtbezirke",
+                text: "Der K\xF6lner Stadtbezirk <b>{NAME}</b>."
+            }
+        },
+        koeln2: {
+            title: "Bildung und Kultur",
+            description: "Liste aller Bibliotheken, Museen und Schulen in Köln.",
+            libraries: {
+                title: "Bibliotheken"
+            },
+            museums: {
+                title: "Museen",
+                text: "Das Museum <b>{NAME}</b> liegt im K\xF6lner Stadtteil {STADTTEIL}."
+            },
+            schools: {
+                title: "Schulen",
+                text: ""
+            }
+        },
+        koeln3: {
+            title: "Freizeit",
+            sights: {
+                title: "Sehensw\xFCrdigkeiten",
+                titleSingle: "Sehensw\xFCrdigkeit",
+                text: "Die Sehensw\xFCrdigkeit <b>{NAME}</b> liegt im K\xF6lner Stadtteil {STADTTEIL}."
+            },
+            playgrounds: {
+                title: "Spiel- und Sportpl\xE4tze",
+                text: "<b>{Spielplatzname}</b> liegt im K\xF6lner Stadtteil {Stadtteil}.",
+                baskets: "Basketballk\xF6rbe",
+                goals: "Fussballtore",
+                tables: "Tischtennis Tische",
+                walls: "Torwand",
+                skate: "Skaten",
+                misc: "Sonstiges"
+            },
+            places: {
+                title: "Veranstaltungsorte",
+                titleSingle: "Veranstaltungsort",
+                text: "<b>{NAME}</b> ist ein {expression/carrier} Veranstaltungsort."
+            }
+        },
+        basemaps: {
+            gray: "Stra\xDFenkarte (grau)",
+            streets: "Stra\xDFenkarte",
+            topo: "Topographische Karte",
+            hybrid: "Luftbild (hybrid)"
+        }
+    },
+    tools: {
+        drawerLeft: "Werkzeuge",
+        measuring: "Messwerkzeuge"
+    },
+    common: {
+        number: "Nummer",
+        area: "Fl\xE4che [ha]",
+        totalArea: "Anteil an Gesamtfl\xE4che [%]",
+        name: "Name",
+        provider: "Tr\xE4ger",
+        address: "Adresse",
+        furtherinfo: "Weitere Informationen",
+        precint: "Stadtbezirk",
+        district: "Stadtviertel",
+        private: "private",
+        municipal: "st\xE4dtischer",
+        zip: "PLZ",
+        type: "Art"
+    }
+};
